18f2cad74e92021a0d0cb355c9baced6f1b29a1a
Darina <39308644+DarinaN@users.noreply.github.com>
2018-09-18 21:45:03 +0300

    Add files via upload

---
+++ java3.js
@@ -0,0 +1,25 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+
+
+function f1()
+ {
+                      if (a != b) 
+		       {a = a + b;
+			b = a;
+		  	System.out.println(a + " " + b);}
+		             
+                        else {
+			a = b = 0;
+			System.out.println(a + " " + b);
+		}
+	}
