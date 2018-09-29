cf9a16a37578dda301f481f5394e7d41cb304418
Darina <39308644+DarinaN@users.noreply.github.com>
2018-09-29 14:04:50 +0300

    Add files via upload

---
+++ java33.js
@@ -0,0 +1,43 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+
+
+function f1()
+{N=parseFloat($('tt1').value);
+ K=parseFloat($('tt2').value);
+ Sum=1;
+ 
+                     for(i=2;i<=N;i++) 
+		       {a=i;
+                           for(j=2;j<=K;j++)
+                       {a=a*i;
+                        Sum=Sum+a;}
+		  	$('tt3').value=Sum;
+                                
+		             }
+
+}
+
+
+function f2()
+{A=parseFloat($('tt4').value);
+ B=parseFloat($('tt5').value);
+ 
+                     for(i=A;i<=B;i++) 
+		       
+                     { 
+                      for(j=0;j<i;j++)
+                     { $('tt6').value=i+" ";
+		  	}
+                        
+}
+}
