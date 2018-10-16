6dd453c7637cf12b96ecd9d700ead761bf650e06
Darina <39308644+DarinaN@users.noreply.github.com>
2018-10-16 19:20:38 +0300

    Update java34.js

--- java34.js
+++ java34.js
@@ -14,8 +14,9 @@ return elements; }
 function f1()
 {N=parseFloat($('tt1').value);
                        K=0;
-                while(Math.pow(3,K) > N)
+                while(Math.pow(3,K) < N)
 		       K++;
+                       K--;
                       $('tt2').value=K;
                              
 
