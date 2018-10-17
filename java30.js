b1aa7ad6ca06ec26475e631e0bb02a683b8048e7
Darina <39308644+DarinaN@users.noreply.github.com>
2018-10-17 12:36:46 +0300

    Update java30.js

--- java30.js
+++ java30.js
@@ -13,7 +13,7 @@ return elements; }
 var setdate = new Date("May 09 1945 00:00:00");
 function f4 ()
 {
-  now = new Date();
+  var now = new Date();
   
   
   day = now.getTime() - setdate.getTime();
