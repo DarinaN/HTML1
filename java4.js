f0ea5060735a9310b00039554b933b4ffac7d625
Darina <39308644+DarinaN@users.noreply.github.com>
2018-10-23 22:02:31 +0300

    Add files via upload

---
+++ java4.js
@@ -0,0 +1,27 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+
+
+function f1()
+    {
+        pravilno = false;
+        if (document.getElementById('In').value == "Львов")
+            { pravilno = true; } else if (document.getElementById('In').value == "Харьков")
+                { pravilno = true; } else if (document.getElementById('In').value == "Киев")
+                   { pravilno = true; } else if (document.getElementById('In').value == "Одесса")
+                       { pravilno = true; } else if (document.getElementById('In').value == "Винница")
+                           { pravilno = true; } else if (document.getElementById('In').value == "Николаев")
+                              { pravilno = true; } else 
+                        { pravilno = false; };
+        if (pravilno == true) { alert("Okay"); } else { alert("Fail"); };
+        return pravilno;
+    }
