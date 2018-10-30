841fb20c41a07c608db2b3614201a9e08ba82223
Darina <39308644+DarinaN@users.noreply.github.com>
2018-10-30 21:56:04 +0200

    Update java30.js

--- java30.js
+++ java30.js
@@ -11,31 +11,19 @@ elements.push(element);
 return elements; }  
 
 function f4(){
+a=parseFloat($('tt1').value);
         setInterval(function(){
-           var lnow = new Date("05 09 1945");   
+            var lnow = new Date("05 09 1945");
                 lyear = lnow.getFullYear(); 
-                lmonth1  = lnow.getMonth() ; 
-                lmonth = (lmonth1+1); 
-                ldate =   lnow.getDate() ;  
+               
+                dnyear = (a - lyear); 
+              
                 
- 
- 
-             var now = new Date();   
-                year = now.getFullYear(); 
-                month1  = now.getMonth() ; 
-                month = (month1+1); 
-                date =   now.getDate() ;  
-                   
- 
-               dnyear = (year - lyear);
-                dnmonth = ( month - lmonth); 
-                dndate = (date - ldate);   
-                
-                
-
-               /*allow = Math.round(-allow);*/
-               $('tt2').value= dnyear + " Лет "  + dnmonth + " Месяц " + dndate + " Дней ";
+            $('tt2').value= dnyear + " Лет "  ;
 }, 1000);
 }
+            
+
+
             
 
