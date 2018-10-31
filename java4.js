aa14bf8c51e1e32062a82f9f9776f46763e06475
Darina <39308644+DarinaN@users.noreply.github.com>
2018-10-31 10:14:00 +0200

    Update java4.js

--- java4.js
+++ java4.js
@@ -13,5 +13,11 @@ return elements; }
 
 function f1()
     {
-       
+       var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+    var myMail = document.getElementById('Result').value;
+    var valid = re.test(myMail);
+    if (valid) output = 'Адрес эл. почты введен правильно!';
+    else output = 'Адрес электронной почты введен неправильно!';
+    document.getElementById('message').innerHTML = output;
+    return valid;
     }
